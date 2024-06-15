b85238be476efc429b543f0af4254ccc86eb2575
Maxime Bertrait <31846522+mbertrait@users.noreply.github.com>
2024-06-15 02:01:32 +0200

    Solve error in swap AF

--- twix_reco.py
+++ twix_reco.py
@@ -2,8 +2,6 @@ import os
 import torch
 import numpy as np
 import scipy
-#import logging
-
 
 from grappaND import GRAPPA_Recon
 from twix_reader import read_twix_datafile
@@ -28,7 +26,7 @@ def _performNoiseDecorr(data, noise):
     return data_decorr
 
 
-def Twix_GRAPPA_Recon(filepath, savepath=None, performRSS=True, verbose=False):
+def Twix_GRAPPA_Recon(filepath, savepath=None, performRSS=True, verbose=True):
     if not savepath or os.path.isdir(savepath):
         base = os.path.splitext(os.path.basename(filepath))[0]
         savepath = os.path.join(os.path.dirname(filepath) if not savepath else savepath, base + "_GRAPPArecon")
@@ -41,10 +39,11 @@ def Twix_GRAPPA_Recon(filepath, savepath=None, performRSS=True, verbose=False):
     data['image'] = _performNoiseDecorr(data['image'], data['noise'])
 
     af = []
-    if ('sPat', 'lAccelFact3D') in scan_info.hdr.MeasYaps:
-        af.append(int(scan_info.hdr.MeasYaps[('sPat', 'lAccelFact3D')]))
+
     if ('sPat', 'lAccelFactPE') in scan_info.hdr.MeasYaps:
         af.append(int(scan_info.hdr.MeasYaps[('sPat', 'lAccelFactPE')]))
+    if ('sPat', 'lAccelFact3D') in scan_info.hdr.MeasYaps:
+        af.append(int(scan_info.hdr.MeasYaps[('sPat', 'lAccelFact3D')]))
 
     rec = GRAPPA_Recon(data['image'], data['refscan'], af=af)
     if verbose: print("GRAPPA Reconstruction Done!")
